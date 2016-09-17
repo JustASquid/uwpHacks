760a262fb6bc753a453d4703fb47c07f6f6743c6
Daniel West <sqiddster@gmail.com>
2016-09-17 17:40:06 -0400

    Added an API call to get similar words based on a source word

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var path = require('path');
+var http = require('http');
 var favicon = require('serve-favicon');
+var URL = require('url');
+var request = require('request');
+var jsdom = require("jsdom");
 
 var app = express();
 
@@ -11,6 +15,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "public/index.html");
+});
+
+app.get("/getWords", function(req, res) {
+    var srcWord = req.query.word;
+    getWords(srcWord, function(result) {
+        res.json(result);
+    });
 });
 
 // catch 404 and forward to error handler
@@ -27,10 +38,6 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
   });
 }
 
@@ -38,10 +45,46 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
+
+var sections = {
+    NOUN: 'nouns',
+    ADJECTIVE: 'adjectives',
+    VERB: 'verbs',
+    ADVERB: 'adverbs'
+};
+
+function getWords(srcWord, resultCallback) {
+    var word = srcWord;
+    var url = "http://wordassociations.net/search?hl=en&w=" + word;
+    jsdom.env({
+        url: url,
+        done: function(error, window) {
+            if (error) {
+                throw new Error("Error loading words");
+            }
+
+            var result = {
+                nouns: [],
+                adjectives: [],
+                verbs: [],
+                adverbs: []
+            };
+
+            for (var section in sections) {
+                var document = window.document;
+                var nounSection = document.getElementsByClassName("NOUN-SECTION")[0];
+                var nounList = nounSection.getElementsByTagName("UL")[0].children;
+                for (var i = 0; i < nounList.length; i++) {
+                    var link = nounList[i].children[0];
+                    var word = link.textContent;
+                    result[sections[section]].push(word);
+                }
+            }
+
+            resultCallback(result);
+        }
+    });
+}
 
 module.exports = app;
